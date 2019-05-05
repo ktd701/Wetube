a0039534405f1be21ff0b13a6dce7583da234ed3
ktd <ktd701@gmail.com>
2019-05-05 16:51:19 +0900

    router and controller

---
+++ js/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import morgan from "morgan"
+import helmet from "helmet"
+import cookieparser from "cookie-parser"
+import bodyparser from "body-parser"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+const app = express(); 
+
+
+app.use(cookieparser());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
---
+++ js/routers/globalRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import routes from "../routes";
+import { home, search, homeController, searchController } from "../controllers/videoController";
+import { join, login, logout, joinController, loginController, logoutController } from "../controllers/userController";
+
+const globalRouter = express.Router();
+globalRouter.get(routes.home, homeController);
+globalRouter.get(routes.join, joinController);
+globalRouter.get(routes.login, loginController);
+globalRouter.get(routes.logout, logoutController);
+globalRouter.get(routes.search, searchController);
+
+export default globalRouter;
